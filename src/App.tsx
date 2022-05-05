8dbff03749ec29cdc0de225fce743b8c194fe11c
thiago baptistella <thiago.baptistella@neilsonfs.com~>
2022-05-05 10:24:26 +0100

    fixed type for math

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const { app } = window.require('@electron/remote')
 const formatSize = (size: number) => {
   var i = Math.floor(Math.log(size) / Math.log(1024))
   return (
-    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    Number((size / Math.pow(1024, i)).toFixed(2)) * 1 +
     ' ' +
     ['B', 'kB', 'MB', 'GB', 'TB'][i]
   )
